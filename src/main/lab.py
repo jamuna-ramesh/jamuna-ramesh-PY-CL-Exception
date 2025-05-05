e6b208fcb4cbbc02a08115499de942f1eef7e411
jamuna-ramesh <jamuna26062002@gmail.com>
2025-05-05 12:47:18 +0530

    Done

--- src/main/lab.py
+++ src/main/lab.py
@@ -21,4 +21,5 @@ class Lab:
     this method has a return type of None - it doesn't need to return anything since the 
     test cases are just checking for a raised exception!
     """
-    
+    def must_throw(self):
+        raise Exception("This method must throw an exception.")
